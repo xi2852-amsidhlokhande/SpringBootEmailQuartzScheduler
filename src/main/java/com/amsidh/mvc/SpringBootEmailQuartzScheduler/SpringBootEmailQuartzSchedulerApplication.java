1e20edac06136294e5ba71fbf6b9d9db4cc76548
amsidh.lokhande@xebia.com <amsidh.lokhande@xebia.com>
2022-04-08 15:07:07 +0530

    Initial commit

---
+++ src/main/java/com/amsidh/mvc/SpringBootEmailQuartzScheduler/SpringBootEmailQuartzSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.amsidh.mvc.SpringBootEmailQuartzScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmailQuartzSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmailQuartzSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amsidh/mvc/SpringBootEmailQuartzScheduler/SpringBootEmailQuartzSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amsidh.mvc.SpringBootEmailQuartzScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEmailQuartzSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
